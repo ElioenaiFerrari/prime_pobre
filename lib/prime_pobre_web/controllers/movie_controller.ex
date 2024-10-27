632fa90d84ad68f849ccf1e12a97321d741c7c36
elioenaiferrari <elioenaiferrari@gmail.com>
2024-10-27 14:46:00 -0300

    file movies

--- lib/prime_pobre_web/controllers/movie_controller.ex
+++ lib/prime_pobre_web/controllers/movie_controller.ex
@@ -13,26 +13,103 @@ defmodule PrimePobreWeb.MovieController do
 
   def stream(conn, %{"id" => id}) do
     with %Movie{} = movie <- Movies.get_movie!(id) do
-      range_header = get_req_header(conn, "range")
-      range_header = if range_header != nil, do: range_header, else: "bytes=0-"
-
-      conn =
-        conn
-        |> put_resp_content_type(movie.mime_type)
-        |> send_chunked(:ok)
-
-      HTTPoison.get!(movie.video_url, [], stream_to: self(), headers: [{"range", range_header}])
-
-      stream_video_chunks(conn)
+      stream_video_chunks(conn, movie)
     end
   end
 
-  defp stream_video_chunks(conn) do
+  defp stream_range(conn, path, start, length) do
+    {:ok, file} = File.open(path, [:read, :binary])
+    :file.position(file, start)
+
+    # Envia o arquivo em blocos a partir do intervalo especificado
+    Enum.reduce_while(0..(length - 1), conn, fn _, conn_acc ->
+      case IO.binread(file, min(1_000_000, length)) do
+        :eof ->
+          {:halt, conn_acc}
+
+        {:error, _} ->
+          {:halt, conn_acc}
+
+        chunk ->
+          case Plug.Conn.chunk(conn_acc, chunk) do
+            {:ok, conn} -> {:cont, conn}
+            {:error, _} -> {:halt, conn_acc}
+          end
+      end
+    end)
+
+    File.close(file)
+    conn
+  end
+
+  defp parse_range(range, file_size) do
+    [start_str, end_str] = String.split(range, "-")
+    start = String.to_integer(start_str)
+    end_pos = if end_str == "", do: file_size - 1, else: String.to_integer(end_str)
+    {start, min(end_pos, file_size - 1)}
+  end
+
+  @video_dir "priv/movies"
+  defp stream_video_chunks(
+         conn,
+         %Movie{
+           video_url: "file://" <> filename
+         }
+       ) do
+    path = Path.join(@video_dir, filename)
+
+    if File.exists?(path) do
+      file_size = File.stat!(path).size
+      range_header = get_req_header(conn, "range")
+
+      # Se existir um cabeçalho "Range", processa o intervalo solicitado
+      conn =
+        case range_header do
+          ["bytes=" <> range] ->
+            {start, end_pos} = parse_range(range, file_size)
+            length = end_pos - start + 1
+
+            # Configura os cabeçalhos de resposta para streaming parcial
+            conn
+            |> put_resp_header("content-type", "video/mp4")
+            |> put_resp_header("content-length", Integer.to_string(length))
+            |> put_resp_header("content-range", "bytes #{start}-#{end_pos}/#{file_size}")
+            |> send_chunked(:partial_content)
+            |> stream_range(path, start, length)
+
+          _ ->
+            # Responde com o vídeo completo se não houver "Range"
+            conn
+            |> put_resp_header("content-type", "video/mp4")
+            |> put_resp_header("content-length", Integer.to_string(file_size))
+            |> send_chunked(:ok)
+            |> stream_range(path, 0, file_size)
+        end
+
+      conn
+    else
+      send_resp(conn, 404, "File not found")
+    end
+  end
+
+  defp stream_video_chunks(
+         conn,
+         %Movie{
+           video_url: "https://" <> _
+         } = movie
+       ) do
+    conn =
+      conn
+      |> put_resp_content_type(movie.mime_type)
+      |> send_chunked(:ok)
+
+    HTTPoison.get!(movie.video_url, [], stream_to: self())
+
     receive do
       %HTTPoison.AsyncChunk{chunk: chunk} ->
         # Envia o chunk ao cliente
         case chunk(conn, chunk) do
-          {:ok, conn} -> stream_video_chunks(conn)
+          {:ok, conn} -> stream_video_chunks(conn, movie)
           {:error, :closed} -> :ok
         end
 
@@ -46,7 +123,7 @@ defmodule PrimePobreWeb.MovieController do
 
       _ ->
         # Ignora qualquer outra mensagem
-        stream_video_chunks(conn)
+        stream_video_chunks(conn, movie)
     after
       # Timeout de 5 segundos
       5_000 -> :ok
